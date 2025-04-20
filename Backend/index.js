ff5dc6333078f58a231cbfefe1a10f90a72f3d65
=abdullahdil <abdullahdilshad111@gmail.com>
2025-04-20 23:12:23 +0500

    Mlops assignment | 01

---
+++ Backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Student = require("./Models/Student");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+// const MONGODB_URL = process.env.URL;
+const MONGODB_URL = "mongodb://localhost:27017/";
+// const port = process.env.port;
+const port = 3001;
+
+const Admin = require("./Routes/Admin");
+app.use(Admin);
+
+app.use(express.json());
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.listen(port, () => {
+  console.log(port);
+});
